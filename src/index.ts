7f481cf12bb49b2ba4ea1a79e1b921846dccd8fb
bhaar <65624699+U-03BB@users.noreply.github.com>
2022-03-31 20:41:05 +0000

    Add test of `Promise.all`

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-// A threadbare test of Typescript compilation
-
-const sleep = (ms: number): Promise<void> => new Promise((res) => setTimeout(res, ms));
+// Simple Functionality Demo
 
 const main = async () => {
-    
-  const oldArray: number[] = [1,2,3]
-
   // Test spread and nullish coalescing operators
+  const oldArray: number[] = [1,2,3]
   const newArray = [...oldArray, undefined ?? 4]
+  console.log(newArray.map((n) => n * 10)); // [10, 20, 30, 40]
 
-  console.log(newArray.map((n) => n * 10));
-
-  const delay = 3;
+  // Test async/await
+  const sleep = (ms: number): Promise<void> => new Promise((res) => setTimeout(res, ms));
+  const delay = 2;
   console.log(`Sleeping for ${delay} seconds.`);
   await sleep(delay * 1000);
   console.log(`Done sleeping.`);
+
+  // Test Promise.all
+  const [a,b] = await Promise.all([Promise.resolve('first-promise'), Promise.resolve('second-promise')]);
+  console.log(`Promise results: ${a} ${b}`);
 }
 
 main();
